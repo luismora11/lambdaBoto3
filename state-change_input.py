1ca6e7428083ab8310d44d9480694de8367c3a45
Luis Morales <luismorales@Luiss-MBP.attlocal.net>
2022-05-31 11:48:29 -0500

    variables

---
+++ state-change_input.py
@@ -0,0 +1,15 @@
+#Input Path
+
+{
+  "instance": "$.detail.instance-id",
+  "region": "$.region",
+  "state": "$.detail.state"
+}
+
+# Input Template
+
+{
+  "instance": <instance>,
+  "region": <region>,
+  "state": <state>
+}
